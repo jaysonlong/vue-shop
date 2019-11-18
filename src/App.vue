be6c03510762644cec082547c5f8bea4aa746625
midmis <2816077691@qq.com>
2019-11-18 21:07:41 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    你好
+    你好!
   </div>
 </template>
 
@@ -12,12 +12,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
