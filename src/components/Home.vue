9185acb7586053cc15ca5fff47d50597745a1dc4
midmis <2816077691@qq.com>
2019-11-19 09:18:55 +0800

    '完成侧边菜单列表'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,147 @@
 <template>
-  <div>Home</div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">注销</el-button>
+    </el-header>
+
+    <el-container>
+      <!-- 侧边导航栏 -->
+      <el-aside :width="isCollapse ? '64px': '200px'">
+        <!-- 折叠按钮 -->
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          :collapse-transition="false"
+          unique-opened
+          default-active="1"
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :collapse="isCollapse">
+          <el-submenu v-for="(menu, index) in menuList"
+            :index="index + ''" :key="menu.id">
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[menu.id]"></i>
+              <!-- 文本 -->
+              <span>{{ menu.authName }}</span>
+            </template>
+
+            <el-menu-item v-for="subMenu in menu.children"
+              :index="'/' + subMenu.path" :key="subMenu.id"
+              @click="saveActivePath('/' + subMenu.path)">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subMenu.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <!-- 主内容区域 -->
+      <el-main>
+        Main
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      menuList: [],
+      iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      isCollapse: false
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
 
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error('获取菜单失败')
+      this.menuList = res.data
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    saveActivePath (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: white;
+  font-size: 20px;
+
+  > div{
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: -moz-none;
+  cursor: pointer;
+
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import axios from 'axios'
 
 // 全局样式表
 import './assets/css/index.css'
 // 阿里巴巴图标库
 import './assets/fonts/iconfont.css'
-// 绑定axios
-import axios from 'axios'
+
+// axios请求处理
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 为每个请求加上Authorization请求头
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
